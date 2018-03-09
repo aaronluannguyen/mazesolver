94059bbd3fc77d19507f0aa45753b9e15baed47a
Anirudh Kumar Subramanyam <ask710@uw.edu>
2018-03-08 23:35:57 -0800

    Complete part 3

--- src/main/java/datastructures/concrete/ArrayDisjointSet.java
+++ src/main/java/datastructures/concrete/ArrayDisjointSet.java
@@ -1,5 +1,7 @@
 package datastructures.concrete;
 
+import datastructures.concrete.dictionaries.ChainedHashDictionary;
+import datastructures.interfaces.IDictionary;
 import datastructures.interfaces.IDisjointSet;
 import misc.exceptions.NotYetImplementedException;
 
@@ -10,27 +12,74 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
     // Note: do NOT rename or delete this field. We will be inspecting it
     // directly within our private tests.
     private int[] pointers;
-
+    private int setIndex;
+    private IDictionary<T, Integer> setItems;
     // However, feel free to add more methods and private helper methods.
     // You will probably need to add one or two more fields in order to
     // successfully implement this class.
 
     public ArrayDisjointSet() {
         // TODO: your code here
+        pointers = new int[100000];
+        setIndex = 0;
+        setItems = new ChainedHashDictionary<T, Integer>();
     }
 
     @Override
     public void makeSet(T item) {
-        throw new NotYetImplementedException();
+        if(setItems.containsKey(item)) {
+            throw new IllegalArgumentException();
+        }
+        
+        if(this.setIndex >= this.pointers.length) {
+            int[] newPointers = new int[this.pointers.length * 2];
+            for(int i = 0; i < this.pointers.length; i++) {
+                newPointers[i] = this.pointers[i];
+            }
+            this.pointers = newPointers;
+        }
+        
+        this.pointers[this.setIndex] = -1;
+        this.setItems.put(item, this.setIndex);
+        this.setIndex++;  
+        
+
     }
 
     @Override
     public int findSet(T item) {
-        throw new NotYetImplementedException();
+        if(!this.setItems.containsKey(item)) {
+            throw new IllegalArgumentException();
+        }
+        return findSetHelper(this.setItems.get(item));
+    }
+    
+    private int findSetHelper(int index) {
+        if(this.pointers[index] < 0) {
+            return index;
+        }
+        return findSetHelper(this.pointers[index]);
     }
 
     @Override
     public void union(T item1, T item2) {
-        throw new NotYetImplementedException();
+        if(!this.setItems.containsKey(item1) || !this.setItems.containsKey(item2)) {
+            throw new IllegalArgumentException();
+        }
+        int item1RootID = findSet(item1);
+        int item2RootID = findSet(item2);
+        
+        if(item1RootID == item2RootID) {
+            throw new IllegalArgumentException();
+        }
+        
+        int rootRank1 = this.pointers[item1RootID];
+        int rootRank2 = this.pointers[item2RootID];
+        if(rootRank1 <= rootRank2) {
+            this.pointers[item2RootID] = item1RootID;
+        }else {
+            this.pointers[item1RootID] = item2RootID;
+        }                        
+        
     }
 }
