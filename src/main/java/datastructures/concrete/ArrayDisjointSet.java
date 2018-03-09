0c13606d39f7298fc771a39ba7db3a40302caa35
aaronluannguyen <aaronluannguyen@gmail.com>
2018-03-08 22:29:33 -0800

    Finished part of disjoint array set

--- src/main/java/datastructures/concrete/ArrayDisjointSet.java
+++ src/main/java/datastructures/concrete/ArrayDisjointSet.java
@@ -1,5 +1,7 @@
 package datastructures.concrete;
 
+import datastructures.concrete.dictionaries.ChainedHashDictionary;
+import datastructures.interfaces.IDictionary;
 import datastructures.interfaces.IDisjointSet;
 import misc.exceptions.NotYetImplementedException;
 
@@ -10,18 +12,26 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
     // Note: do NOT rename or delete this field. We will be inspecting it
     // directly within our private tests.
     private int[] pointers;
+    private int setIndex;
+    private IDictionary<T, Integer> setItems;
 
     // However, feel free to add more methods and private helper methods.
     // You will probably need to add one or two more fields in order to
     // successfully implement this class.
 
     public ArrayDisjointSet() {
-        // TODO: your code here
+        pointers = new int[100];
+        setIndex = 0;
+        setItems = new ChainedHashDictionary<T, Integer>();
     }
 
     @Override
     public void makeSet(T item) {
-        throw new NotYetImplementedException();
+        if (setItems.containsKey(item)) {
+            throw new IllegalArgumentException("Illegal Argument Exception: Item already a part of this disjoing set");
+        }
+        pointers[this.setIndex] = -1;
+        setItems.put(item, this.setIndex);
     }
 
     @Override
