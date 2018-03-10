af6f3a9d758fb2b46470142f19ca73535ca2c24f
aaronluannguyen <aaronluannguyen@gmail.com>
2018-03-10 00:39:49 -0800

    Finished implementing the kruskal maze carver

--- src/main/java/mazes/generators/maze/KruskalMazeCarver.java
+++ src/main/java/mazes/generators/maze/KruskalMazeCarver.java
@@ -1,9 +1,13 @@
 package mazes.generators.maze;
 
+import java.util.Random;
+
+import datastructures.concrete.ChainedHashSet;
 import datastructures.interfaces.ISet;
 import mazes.entities.Maze;
+import mazes.entities.Room;
 import mazes.entities.Wall;
-import misc.exceptions.NotYetImplementedException;
+import misc.graphs.Graph;
 
 /**
  * Carves out a maze based on Kruskal's algorithm.
@@ -18,6 +22,24 @@ public class KruskalMazeCarver implements MazeCarver {
         // In particular, if you call 'wall.setDistance()' at any point, make sure to
         // call 'wall.resetDistanceToOriginal()' on the same wall before returning.
 
-        throw new NotYetImplementedException();
+        ISet<Room> rooms = maze.getRooms();
+        ISet<Wall> walls = maze.getWalls();
+        ISet<Wall> untouchableWalls = maze.getUntouchableWalls();
+        
+        Random rand = new Random();
+        ISet<Wall> randomWalls = new ChainedHashSet<Wall>();
+        for (Wall wall : walls) {
+            if (!untouchableWalls.contains(wall)) {
+                double randDist = rand.nextDouble();
+                wall.setDistance(randDist);
+                randomWalls.add(wall);
+                wall.resetDistanceToOriginal();    
+            }
+        }
+        
+        Graph<Room, Wall> graph = new Graph<Room, Wall>(rooms, randomWalls);
+        ISet<Wall> mstWalls = graph.findMinimumSpanningTree();
+        
+        return mstWalls;
     }
 }
